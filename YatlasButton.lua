f801428b1863d44db49153ab7df9704eda17a625
Dramacydal <PulLumBerMal@gmail.com>
2026-08-09 04:25:29 +0300

    implement generic map button

--- YatlasButton.lua
+++ YatlasButton.lua
@@ -1,62 +1,34 @@
+local LDB = LibStub("LibDataBroker-1.1");
+local icon = LibStub("LibDBIcon-1.0");
 
-
-function YatlasButton_OnEvent(event) 
-    if(event == "VARIABLES_LOADED") then
-        YatlasOptionsButtonSlider:SetValue(YatlasOptions.ButtonLocation);
-        YatlasButton_Update();
-    end
-end
+local YatlasLDB = LDB:NewDataObject("Yatlas", {
+    type = "launcher",
+    icon = "Interface\\AddOns\\Yatlas\\images\\Button",
+    OnClick = function(self, button)
+        YatlasFrame:Toggle();
+    end,
+    OnTooltipShow = function(tooltip)
+        tooltip:AddLine(YATLAS_BUTTON_TOOLTIP1);
+        tooltip:AddLine(YATLAS_BUTTON_TOOLTIP2);
+        tooltip:AddLine(YATLAS_BUTTON_TOOLTIP3);
+    end,
+});
 
 function YatlasButton_Update()
     if(YatlasOptions.ShowButton) then
-        YatlasButtonFrame:SetPoint(
-            "TOPLEFT", "Minimap", "TOPLEFT",
-            54 - (78 * cos(YatlasOptions.ButtonLocation)),
-            (78 * sin(YatlasOptions.ButtonLocation)) - 55);
-        YatlasButtonFrame:Show();
+        icon:Show("Yatlas");
     else
-        YatlasButtonFrame:Hide();
+        icon:Hide("Yatlas");
     end
 end
 
-function YatlasButton_BeingDragged()
-    -- Thanks to Gello for this code
-    local xpos,ypos = GetCursorPosition() 
-    local xmin,ymin = Minimap:GetLeft(), Minimap:GetBottom() 
-
-    xpos = xmin-xpos/UIParent:GetScale()+70 
-    ypos = ypos/UIParent:GetScale()-ymin-70 
-
-    YatlasButton_SetPosition(math.deg(math.atan2(ypos,xpos)));
-end
-
-function YatlasButton_OnEnter(self)
-    GameTooltip:SetOwner(self, "ANCHOR_LEFT");
-    GameTooltip:SetText(YATLAS_BUTTON_TOOLTIP1);
-    GameTooltip:AddLine(YATLAS_BUTTON_TOOLTIP2);
-    GameTooltip:AddLine(YATLAS_BUTTON_TOOLTIP3);
-    GameTooltip:Show();
-end
-
-function YatlasButton_SetPosition(v)
-    if(v < 0) then
-        v = v + 360;
-    end
-    if(v == YatlasOptions.ButtonLocation) then
-        return;
+local buttonframe = CreateFrame("Frame");
+buttonframe:RegisterEvent("VARIABLES_LOADED");
+buttonframe:SetScript("OnEvent", function(self, event, ...)
+    if(YatlasOptions.MinimapButton == nil) then
+        YatlasOptions.MinimapButton = {};
     end
 
-    YatlasOptionsButtonSlider:SetValue(YatlasOptions.ButtonLocation);
-
-    YatlasOptions.ButtonLocation = v;
-    YatlasButton_Update()
-end
-
-function YatlasButton_GetPosition()
-    return YatlasOptions.ButtonLocation;
-end
-
-
-function YatlasButton_OnClick()
-    YatlasFrame:Toggle();
-end
+    icon:Register("Yatlas", YatlasLDB, YatlasOptions.MinimapButton);
+    YatlasButton_Update();
+end);
